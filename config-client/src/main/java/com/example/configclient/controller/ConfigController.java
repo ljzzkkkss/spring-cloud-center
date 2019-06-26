b546871b03bd9c2e65848adfb910ebb193de17c5
ljzzkkkss <793958447@qq.com>
2019-06-26 23:29:02 +0800

    first commit

---
+++ config-client/src/main/java/com/example/configclient/controller/ConfigController.java
@@ -0,0 +1,16 @@
+package com.example.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+    @Value("${foo}")
+    private String foo;
+
+    @GetMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
